b49adb99bbcc3a706f0c768681e2f78ae325bad9
Lam Nhan <lamnhan92@gmail.com>
2020-10-26 11:53:00 +0700

    update help service

--- src/lib/services/help.service.ts
+++ src/lib/services/help.service.ts
@@ -10,14 +10,15 @@ export class HelpService {
   Global commands:
 
     ${green('google [subcommand]')} ........ Manage Google accounts.
+    ${green('project [subcommand]')} ....... Project general tasks.
     ${green('database [subcommand]')} ...... Manage the database.
     ${green('new [name] [source]')} ........ Start a new project.
     ${green('docs')} ....................... Open the documentation.
     ${green('update')} ..................... Check and install update.
     ${green('help')} ....................... Display help.
-    ${green('<cmd>')} ...................... Run unknown commands.
+    ${green('<cmd>')} ...................... Run any command.
 
-  Project commands:
+  Project related commands:
 
     ${green('new')} ........................ Start a new project.
     ${green('setup')} ...................... Setup the project.
@@ -26,9 +27,11 @@ export class HelpService {
     ${green('urls')} ....................... View project URLs.
     ${green('url [name]')} ................. View or open a project URL.
     ${green('info')} ....................... Output project info.
+    ${green('lint')} ....................... Lint the project.
+    ${green('test')} ....................... Test the project.
     ${green('build')} ...................... Build the project.
-    ${green('deploy')} ..................... Deploy the project.
     ${green('preview')} .................... Preview the project.
+    ${green('deploy')} ..................... Deploy the project.
     ${green('backend [subcommand]')} ....... Run backend related commands.
     ${green('frontend [subcommand]')} ...... Run frontend related commands.`
     );
@@ -43,12 +46,15 @@ export class HelpService {
     ${green('docs')} ....................... Open the documentation.
     ${green('update')} ..................... Check and install update.
     ${green('help')} ....................... Display help.
-    ${green('<cmd>')} ...................... Run unknown commands.
+    ${green('<cmd>')} ...................... Run any command.
 
   Command groups:
 
     ${green('google [subcommand]')} ........ Manage Google accounts.
     ${green('project [subcommand]')} ....... Project general tasks.
+    ${green('config [subcommand]')} ........ Config the project.
+    ${green('backend [subcommand]')} ....... Backend related tasks.
+    ${green('frontend [subcommand]')} ...... Frontend related tasks.
     ${green('database [subcommand]')} ...... Manage the database.
 
   Google sub-commands:
@@ -56,6 +62,15 @@ export class HelpService {
 
   Project sub-commands:
   ${this.projectHelp()}
+
+  Config sub-commands:
+  ${this.configHelp()}
+
+  Backend sub-commands:
+  ${this.backendHelp()}
+
+  Frontend sub-commands:
+  ${this.frontendHelp()}
 
   Database sub-commands:
   ${this.databaseHelp()}`
@@ -77,18 +92,59 @@ export class HelpService {
     return (
       '' +
       `
-    ${green('new [name] [source]')} ........ Start a new project.
     ${green('setup')} ...................... Setup the project.
     ${green('configs')} .................... View project configs.
-    ${green('config [subcommand]')} ........ Config the project.
     ${green('urls')} ....................... View project URLs.
     ${green('url [name]')} ................. View or open a project URL.
     ${green('info')} ....................... Output project info.
+    ${green('lint')} ....................... Lint the project.
+    ${green('test')} ....................... Test the project.
     ${green('build')} ...................... Build the project.
-    ${green('deploy')} ..................... Deploy the project.
     ${green('preview')} .................... Preview the project.
-    ${green('backend [subcommand]')} ....... Run backend related commands.
-    ${green('frontend [subcommand]')} ...... Run frontend related commands.`
+    ${green('deploy')} ..................... Deploy the project.`
+    );
+  }
+
+  configHelp() {
+    return (
+      '' +
+      `
+    ${green('list')} ....................... List configurations.
+    ${green('update')} ..................... Update configurations.
+    ${green('import')} ..................... Import configurations.
+    ${green('export')} ..................... Export configurations.`
+    );
+  }
+
+  backendHelp() {
+    return (
+      '' +
+      `
+    ${green('lint')} ....................... Lint the backend.
+    ${green('test')} ....................... Test the backend.
+    ${green('build')} ...................... Build the backend.
+    ${green('push')} ....................... Push the backend.
+    ${green('deploy')} ..................... Deploy the backend.
+    ${green('install')} .................... Install backend dependencies.
+    ${green('uninstall')} .................. Uninstall backend dependencies.
+    ${green('run')} ........................ Run backend scripts.
+    ${green('<cmd>')} ...................... Run any backend command.`
+    );
+  }
+
+  frontendHelp() {
+    return (
+      '' +
+      `
+      ${green('lint')} ....................... Lint the frontend.
+      ${green('test')} ....................... Test the frontend.
+      ${green('build')} ...................... Build the frontend.
+      ${green('prerender')} .................. Prerender the frontend.
+      ${green('deploy')} ..................... Deploy the frontend.
+      ${green('install')} .................... Install frontend dependencies.
+      ${green('uninstall')} .................. Uninstall frontend dependencies.
+      ${green('run')} ........................ Run frontend scripts.
+      ${green('<cmd>')} ...................... Run any frontend command.`
     );
   }
 
